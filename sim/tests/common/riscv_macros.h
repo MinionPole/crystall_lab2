570faff3bed57a5603fc4cef1d3cf2cdd0f4d2c4
Roman <roma.yunusov.2001@mail.ru>
2025-12-01 14:31:41 +0300

    выполнена работа

--- sim/tests/common/riscv_macros.h
+++ sim/tests/common/riscv_macros.h
@@ -102,8 +102,12 @@
 #define INTERRUPT_HANDLER j other_exception /* No interrupts should occur */
 
 #define RVTEST_CODE_BEGIN                                               \
+        .org 0x1000,0;                                                  \
+        .section .rodata;                                               \
+        .balign 4;                                                      \
+        MSG_TRAP:                                                       \
+        .asciz "illegal trap";                                          \
         .section .text.init;                                            \
-        .org 0xC0, 0x00;                                                \
         .balign  64;                                                    \
         .weak stvec_handler;                                            \
         .weak mtvec_handler;                                            \
@@ -116,6 +120,18 @@ trap_vector:                                                            \
         beq a4, a5, _report;                                            \
         li a5, CAUSE_MACHINE_ECALL;                                     \
         beq a4, a5, _report;                                            \
+        li a5, CAUSE_ILLEGAL_INSTRUCTION;                                     \
+        bne a4, a5, skip_error_print;                                            \
+        /* init for loop, 0xf0000000 address for print */               \
+        lui a6, 0xf0000;                                                \
+        la a7, MSG_TRAP;                                                \
+next_iter:                                                              \
+        lb a5, 0(a7);                                                   \
+        beq a5, x0, break_from_loop;                                    \
+        sw a5, 0(a6);   /* write to a6 char for print */                \
+        addi a7, a7, 1;                                                 \
+        jal x0,next_iter;                                               \
+break_from_loop:                                                        \
         /* if an mtvec_handler is defined, jump to it */                \
         la a4, mtvec_handler;                                           \
         beqz a4, 1f;                                                    \
@@ -124,6 +140,8 @@ trap_vector:                                                            \
 1:      csrr a4, mcause;                                                \
         bgez a4, handle_exception;                                      \
         INTERRUPT_HANDLER;                                              \
+skip_error_print:                                                   \
+        mret;                                                           \
 handle_exception:                                                       \
         /* we don't know how to handle whatever the exception was */    \
 other_exception:                                                        \
@@ -133,6 +151,7 @@ _report:                                                                \
         j sc_exit;                                                      \
         .balign  64;                                                    \
         .globl _start;                                                  \
+        .section .text.start;                                           \
 _start:                                                                 \
         RISCV_MULTICORE_DISABLE;                                        \
         /*INIT_SPTBR;*/                                                 \
